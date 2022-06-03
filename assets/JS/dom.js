60b8ad31de56bec4faa6273591ffa5bd1ee958b5
Emanuele <emanuelefotia2@gmail.com>
2022-06-03 15:48:27 +0200

    emmet

--- assets/JS/dom.js
+++ assets/JS/dom.js
@@ -1,99 +1,99 @@
 //creo la navbar di bootstrap
-const nav = document.createElement("nav");
-nav.classList.add("navbar","bg-transparent");
-const cont = document.getElementById("cont");
+const nav = document.createElement('nav');
+nav.classList.add('navbar', 'bg-transparent');
+const cont = document.getElementById('cont');
 cont.appendChild(nav);
 
 //div principale
-const mainDiv = document.createElement("div");
-mainDiv.style.textAlign = "center";
+const mainDiv = document.createElement('div');
+mainDiv.style.textAlign = 'center';
 nav.appendChild(mainDiv);
 
 //h1
-const h1 = document.createElement("h1");
-h1.classList.add("h1-responsive","font-weight-bold","text-center","my-4");
-h1.style.fontWeight = "bold";
+const h1 = document.createElement('h1');
+h1.classList.add('h1-responsive', 'font-weight-bold', 'text-center', 'my-4');
+h1.style.fontWeight = 'bold';
 mainDiv.appendChild(h1);
-const testo = document.createTextNode("Life Quality");
+const testo = document.createTextNode('Life Quality');
 h1.append(testo);
 
 //div2
-const div2 = document.createElement("div");
-div2.style.textAlign = "center";
+const div2 = document.createElement('div');
+div2.style.textAlign = 'center';
 mainDiv.appendChild(div2);
 
 //h2
-const h2 = document.createElement("h2");
-h2.classList.add("h1-responsive","font-weight-bold","text-center","my-4");
-h2.style.fontWeight = "bold";
+const h2 = document.createElement('h2');
+h2.classList.add('h1-responsive', 'font-weight-bold', 'text-center', 'my-4');
+h2.style.fontWeight = 'bold';
 div2.appendChild(h2);
-const testo2 = document.createTextNode("Enter the city and find out");
+const testo2 = document.createTextNode('Enter the city and find out');
 h2.append(testo2);
 
 //div3
-const div3 = document.createElement("div");
-div3.classList.add("container-fluid");
+const div3 = document.createElement('div');
+div3.classList.add('container-fluid');
 div2.appendChild(div3);
 
 //clear button
-const btn = document.createElement("button");
-btn.classList.add("btn","btn-outline-success");
-btn.id = "clear";
+const btn = document.createElement('button');
+btn.classList.add('btn', 'btn-outline-success');
+btn.id = 'clear';
 div3.appendChild(btn);
-const testoBtn = document.createTextNode("Clear");
+const testoBtn = document.createTextNode('Clear');
 btn.append(testoBtn);
 
 //input form
-const input = document.createElement("input");
-input.classList.add("form-control","me-2");
-input.type = "text";
-input.placeholder = "e.g: rome";
-input.id = "city";
+const input = document.createElement('input');
+input.classList.add('form-control', 'me-2');
+input.type = 'text';
+input.placeholder = 'e.g: rome';
+input.id = 'city';
 div3.appendChild(input);
 
 //search button
-const btn2 = document.createElement("button");
-btn2.classList.add("btn","btn-outline-success");
-btn2.id = "search";
+const btn2 = document.createElement('button');
+btn2.classList.add('btn', 'btn-outline-success');
+btn2.id = 'search';
 div3.appendChild(btn2);
-const testoBtn2 = document.createTextNode("Search");
+const testoBtn2 = document.createTextNode('Search');
 btn2.append(testoBtn2);
 
 //div risultati
-const div4 = document.createElement("div");
-div4.classList.add("container");
-const body = document.getElementById("body");
+const div4 = document.createElement('div');
+div4.classList.add('container');
+const body = document.getElementById('body');
 body.appendChild(div4);
 
 //row div
-const div5 = document.createElement("div");
-div5.classList.add("row");
+const div5 = document.createElement('div');
+div5.classList.add('row');
 div4.appendChild(div5);
 
 //div sum
-const divSum = document.createElement("div");
-divSum.classList.add("col-sm");
+const divSum = document.createElement('div');
+divSum.classList.add('col-sm');
 div5.appendChild(divSum);
 
 //p sum
-const pSum = document.createElement("p");
-pSum.classList.add("para");
-pSum.id = "sum";
+const pSum = document.createElement('p');
+pSum.classList.add('para');
+pSum.id = 'sum';
 divSum.appendChild(pSum);
 
 //div cat
-const divCat = document.createElement("div");
-divCat.classList.add("col-sm","mb-3");
-divCat.id = "cat";
+const divCat = document.createElement('div');
+divCat.classList.add('col-sm', 'mb-3');
+divCat.id = 'cat';
 div5.appendChild(divCat);
 
 //div score
-const divScore = document.createElement("div");
-divScore.classList.add("col-sm");
+const divScore = document.createElement('div');
+divScore.classList.add('col-sm');
 div5.appendChild(divScore);
 
 //p score
-const pScore = document.createElement("p");
-pScore.classList.add("para");
-pScore.id = "score";
+const pScore = document.createElement('p');
+pScore.classList.add('para');
+pScore.id = 'score';
 divScore.appendChild(pScore);
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,72 +1,66 @@
 //dichiaro tutti gli elementi da intercettare
-const city = document.getElementById("city");
-const search = document.getElementById("search");
-const clear = document.getElementById("clear");
-const sum = document.getElementById("sum");
-const categories = document.getElementById("cat");
-const score = document.getElementById("score");
+const city = document.getElementById('city');
+const search = document.getElementById('search');
+const clear = document.getElementById('clear');
+const sum = document.getElementById('sum');
+const categories = document.getElementById('cat');
+const score = document.getElementById('score');
 
 //apro la funzione per mandare la richiesta alle API di teleport
 
-const api = async function(city){
-const request = await axios
-  .get("https://api.teleport.org/api/urban_areas/slug:"+city+"/scores/")
-  .then(response => response);
-  console.log(request);
+const api = async function(city) {
+	const request = await axios
+		.get('https://api.teleport.org/api/urban_areas/slug:' + city + '/scores/')
+		.then(response => response);
+	console.log(request);
 
+	//descrizione città
+	const summary = request.data.summary;
+	console.log(summary);
+	sum.insertAdjacentHTML('afterbegin', summary);
 
+	//score di ogni categoria della città
+	const sectionCategories = request.data.categories.forEach(element => {
+		let section = document.createElement('section');
+		section.classList.add('section_categories');
+		categories.appendChild(section);
+		let create = document.createElement('div');
+		create.classList.add('categories');
+		section.appendChild(create);
 
-//descrizione città
-  const summary = (request.data.summary);
-  console.log(summary);
-  sum.insertAdjacentHTML ("afterbegin", summary);
+		const span = document.createElement('span');
+		const title = span.append(element.name);
+		create.append(span);
+		const change = element.score_out_of_10.toFixed(1) * 10;
+		console.log(change);
 
-//score di ogni categoria della città
-  const sectionCategories = request.data.categories.forEach(element =>{
-    let section = document.createElement("section");
-    section.classList.add("section_categories");
-    categories.appendChild(section);
-    let create = document.createElement("div");
-    create.classList.add("categories");
-    section.appendChild(create);
+		const progressContainer = document.createElement('div');
+		const bar = document.createElement('div');
 
-    const span = document.createElement("span");
-    const title = span.append(element.name);
-    create.append(span);
-    const change = element.score_out_of_10.toFixed(1) * 10;
-    console.log(change);
+		create.appendChild(progressContainer);
+		const progressBar = progressContainer.appendChild(bar);
+		bar.classList.add('progress-bar', 'progress-bar-striped', 'bg-success', 'progress-bar-animated');
+		bar.innerHTML = `${change}/100`;
+		progressContainer.classList.add('progress');
+		progressBar.style.width = `${change}%`;
+	});
 
-
-    const progressContainer = document.createElement("div");
-    const bar = document.createElement("div");
-
-    create.appendChild(progressContainer);
-    const progressBar = progressContainer.appendChild(bar);
-    bar.classList.add("progress-bar","progress-bar-striped","bg-success","progress-bar-animated");
-    bar.innerHTML=(`${change}/100`);
-    progressContainer.classList.add("progress");
-    progressBar.style.width =  `${change}%`;
-
-  });
-
-//score generale della città
-  const telescore = (request.data.teleport_city_score);
-  console.log(telescore);
-  score.insertAdjacentHTML ("afterbegin", "<b>Teleport city score : </b>" + telescore);
-
+	//score generale della città
+	const telescore = request.data.teleport_city_score;
+	console.log(telescore);
+	score.insertAdjacentHTML('afterbegin', '<b>Teleport city score : </b>' + telescore);
 };
 
 //manda a schermo i risultati della richiesta fatta alle api di teleport
-search.addEventListener("click", async function () {
-  sum.innerHTML = " ";
-  categories.innerHTML = " ";
-  score.innerHTML = " ";
-  api(city.value);
-})
-
+search.addEventListener('click', async function() {
+	sum.innerHTML = ' ';
+	categories.innerHTML = ' ';
+	score.innerHTML = ' ';
+	api(city.value);
+});
 
 //ripulisce il form e refresha la pagina
-clear.addEventListener("click",function(){
-  location.reload();
-  city.value = "";
-})
+clear.addEventListener('click', function() {
+	location.reload();
+	city.value = '';
+});
